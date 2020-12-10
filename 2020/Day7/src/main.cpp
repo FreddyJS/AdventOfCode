25411fdaa208d3826f99527a41f3d0f2966c35ce
Freddy <sebashgt@gmail.com>
2020-12-10 01:26:19 +0100

    Improved to read just one time the input.txt file

--- 2020/Day7/src/main.cpp
+++ 2020/Day7/src/main.cpp
@@ -56,59 +56,69 @@ bool checkIfContain(string line, string search, int n) {
     return false;
 }
 
-void checkBags(int argc, char* argv[]) {
-    ifstream clusterFile;
-	clusterFile.open(argv[1]);
-
+void checkBags(LinkedList<string>* input) {
 	string readed;
     int n_bags = 0;
-	int n_line = 1;
 
     string sgb = "shiny gold bag";
 
-	while(getline(clusterFile, readed)) {
-        char line[readed.length()];
-        strcpy(line, readed.c_str());
-        int len = strlen(line);
+	for (size_t i = 0; i < input->size; i++) {
+        readed = input->get(i);
+        bool canContain = checkIfContain(readed, sgb, 1);
 
-
-        if (len > 0 || clusterFile.eof()) {
-            bool canContain = checkIfContain(readed, sgb, 1);
-
-            if (canContain) {
-                n_bags++;
-            }
+        if (canContain) {
+            n_bags++;
         }
-
-        n_line++;
     }
 
-	cout << "Readed " << n_line << " lines!" << endl;
-    cout << "The number of bags that can contain shiny gold bags are: " << n_bags << "\n" << endl;
+    
 
-    clusterFile.close();
+    cout << "The number of bags that can contain shiny gold bags are: " << n_bags << endl;
 }
 
 int main(int argc, char* argv[]) {
-	cout << "Hey, im dope, give me some clusters!" << endl;
-
 	if (argc != 2) {
-		cout << "Give me a file with the cluster(s)'s points!" << endl;
+		cout << "Give me a file with to use as the input data!" << endl;
 	} else {
-		cout << "Thanks! I will analyze the file: " << argv[1] << "\n" << endl;
+		cout << "The input data file is: " << argv[1] << endl;
 	}
+
+    LinkedList<string>* input = new LinkedList<string>();
+
+    string readed;
+	int n_line = 1;
+
+    ifstream clusterFile;
+	clusterFile.open(argv[1]);
+
+	while(getline(clusterFile, readed)) {
+
+        if (readed.length() > 0 || clusterFile.eof()) {
+            input->addLast(readed);
+        }
+        
+        n_line++;
+    }
+
+	cout << "Readed " << n_line << " lines from " << argv[1] << "\n" << endl;
+
+    clusterFile.close();
 
     size_t n_bags = bags->size;
     int n_loops = 1;
+    cout << "\t\t\033[1;34m---------- Day 7 Part 1 ----------\033[0m" << endl;
     do {
         cout << "Loop " << n_loops << ": ";
         n_bags = bags->size;
-        checkBags(argc, argv);
+        checkBags(input);
 
         n_loops++;
     } while(n_bags != bags->size);
 
-    cout << "The list has: " << bags->size << " bags!" << endl;
+    cout << "\033[1;31mThe final number of bags that can contain shiny gold bags are: " << bags->size << " bags!\n\033[0m" << endl;
+
+    cout << "\t\t\033[1;34m---------- Day 7 Part 2 ----------\033[0m" << endl;
+
     bags->destruct();
 
     return 0;
